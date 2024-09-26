e73653fa46badfb7bedfeaa5bfc7a365dc48bce0
VENKATARAMANA BARATAM <109843726+Venkataramanabaratam1@users.noreply.github.com>
2024-09-26 17:29:48 +0530

    Submission of Partial Code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from "./components/header/header.js"
+import Body from "./components/body/body.js"
+
+
+function App() {
+  return (
+    <div className="App">
+    <Header />
+    <Body />
+    </div>
+  );
+}
+
+export default App;
